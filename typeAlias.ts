27c35161318a239c1a525221902b05b7ad5693f2
abhi-develope <prajapatiabhishek320@gmail.com>
2024-09-05 08:48:35 +0530

    provided one or option on type declaration

--- typeAlias.ts
+++ typeAlias.ts
@@ -9,3 +9,24 @@
     age: 12,
     address: "ram nager"
  }
+
+
+
+
+ function login(userData: User): User {  //set function return type
+    return userData;
+ }
+
+
+
+
+
+
+ console.log(login(user));  // argument must be pass
+
+
+
+ type ID = number | string;  // one or more option
+
+ const userId: ID = 23  
+ 
