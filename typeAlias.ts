b1e84aefe9ddac832407fc6c68c6120c9d1fcd9f
abhi-develope <prajapatiabhishek320@gmail.com>
2024-09-05 08:31:10 +0530

    typeAlias

---
+++ typeAlias.ts
@@ -0,0 +1,11 @@
+ type User = {
+    name : string;
+    age : number;
+    address?: string;  // ? for optional feild
+ }
+
+ const user: User = {
+    name: "abhishek",
+    age: 12,
+    address: "ram nager"
+ }
