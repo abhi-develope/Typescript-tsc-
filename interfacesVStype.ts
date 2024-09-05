775108b44ec9b0bd2bfe691d81b4662a21e6e599
abhi-develope <prajapatiabhishek320@gmail.com>
2024-09-05 17:52:37 +0530

    interfaces vs type

---
+++ interfacesVStype.ts
@@ -0,0 +1,20 @@
+//------ no merging is possible in type declaration
+
+// type Fruit ={
+//     name: string;
+//     color: string;
+// }
+
+
+// type Fruit {
+//     price: number;
+// }
+
+//------------
+
+
+// -----to create custome premitive we have to use type declaration
+
+type Osem = string;
+type Black = number;
+
